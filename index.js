fa573c9e72d8871bd8ad5864c43f0d9aa481a0b9
Nandana-pradeep33 <95542226+Nandana-pradeep33@users.noreply.github.com>
2023-09-07 17:52:34 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ const {
 require("dotenv").config();
 
 const app = express();
-
+const cors = require("cors");
 app.use(express.json({ limit: "50mb" }));
-
+app.use(cors());
+app.get("/",(req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.send("Api is running");
+});
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
